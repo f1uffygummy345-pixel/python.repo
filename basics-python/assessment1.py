fa04a7c2cce4dc6f4d8e475f8947508253663ca6
f1uffygummy345-pixel <f1uffygummy345@gmail.com>
2026-09-14 10:53:29 -0600

    Add files via upload

---
+++ basics-python/assessment1.py
@@ -0,0 +1,38 @@
+print("Practise Assessment 1")
+
+student_name = "Emma"
+Student_name = "Gabby"
+
+print(f"Name is {student_name} and \nmy friend's name is {Student_name}")
+#Python treats these two values as different because one variable starts with a capital s and the other starts with a lowercase s, therfore making them different.
+
+print('My friends are amazing!')
+print("My friend's amazing!")
+print("Python Basics\n\tStrings are fun!\n\"Practice makes progress.\"")
+
+first_number = "10"
+second_number = "5"
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+
+first_number = 10
+second_number = 5
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+
+#The first result is different because the numbers are stored as strings, so they are concatenated
+
+first_name = "Gabby"
+age = 20
+print(f"{first_name} is {age} years old.")
+
+COURSE_NAME = "Introduction to Python"
+student_name = "Emma"
+current_grade_percentage = "85%"
+print(f"{student_name} has {current_grade_percentage} in {COURSE_NAME}.")
+
+name = input("Enter your name: ")
+print(f"Welcome {name}")
+COURSE_NAME = input("Enter your course name: ")
+print(f"Welcome to {COURSE_NAME}")
+favourite_language = input("Enter your favourite programming language: ")
+print(f"Student Information\n\tName: {name}\n\tProgram: {COURSE_NAME}\n\tFavourite language: {favourite_language}")
+
---
+++ basics-python/pythonbasics.py
@@ -0,0 +1,113 @@
+print("Intro to Python")
+# Comment - does not execute
+# Describes your code for others to read
+
+# Multi lines
+# comment Use
+# ctrl /
+
+#Case Sensitive
+# Extra spaces in your code do not usually matter
+# Strings can use "" or ''
+
+#print('Let's go crazy')
+print ("Hello")
+print("Let's go crazy")
+
+# Escape character/sequences - provide a way to perform an action IN a string:
+
+print('It\'s a "groovy" day!')
+print("It's a \"groovy\" day!")
+print("Hello\n World") #New line
+print("Name:\tEmma") # Tab
+
+print("To go to a new line in a string use \\n")
+
+# Variables
+# a named box or container that holds a value
+# value can change
+# use snake_case for the names
+# names can only contain letters, numbers, and underscores. Cannot start with a number.
+# not explicitly declared in python
+
+# datatypes
+first_name = "Emma" #string
+age = 18 #integer
+price = 12.23 # float
+is_valid = True #boolean (True/False)
+
+# String formatting
+# String concatenation
+print("Your name is " + first_name + " and you are " + str(age) + " years young.")
+
+
+#OR 
+print("Your name is",first_name,"and you are",age,"years young.")
+
+#OR (Best Way)
+print(f"Your name is {first_name} and you are {age} years young.")
+
+#Constants
+#like a variable that does not change
+# Use SCREAMING_SNAKE_CASE
+GST_RATE = 0.05
+subtotal = 100
+total = subtotal * 0.05
+total = subtotal * GST_RATE
+
+# Input from user
+# Input always returns a string
+name = input("Enter your name: ")
+print(f"Welcome {name}")
+print(f"Welcome {name}")
+
+#add numbers and display the sum
+number1 = input("Enter number 1: ")
+number2 = input("Enter number 2: ")
+
+sum = int(number1) + int(number2) # pyright: ignore[reportUndefinedVariable]
+print(f"{number1} + {number2} = {sum}")
+#OR
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+
+sum = number1 + number2
+print(f"{number1} + {number2} = {sum}")
+
+#Prompt the user for two numbers and place them in 2 variables
+#print out the values in the variables
+    #Number1: 20
+    #Number2: 40
+#Swap the numbers in the variables so the value in number2 is the value from number one and vice versa
+#print out the variables
+    #Number1: 40
+    #Number2: 20 
+
+number1 = input("Enter number 1: ")
+number2 = input("Enter number 2: ")
+print(f"Number1: {number1}")
+print(f"Number2: {number2}")
+temp = number1
+number1 = number2
+number2 = temp
+print(f"Number1: {number1}")
+print(f"Number2: {number2}")
+
+
+
+#Math Operators
+print(4+2) #6
+print(4-2) #2
+print(4*2) #8
+print(4/2) #2.0
+print(9//4) #floor division (rounds down)
+print(2**3) #8 exponents
+print(9%4) #1 modulus
+
+#Formatting numbers
+total = 100.1234567
+
+print(round(total, 2)) #100.12
+print(round(total, 6)) #100.123457
+
+print(f"{total:.2f}") #100.12
