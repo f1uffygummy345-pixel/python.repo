d3dfeb3df3b437e394006a0ea74f6d8feef3f893
Emma Smitten <esmitten1@nait.ca>
2026-09-16 11:32:46 -0600

    finished in class demo

--- decisions.py
+++ decisions.py
@@ -4,3 +4,25 @@ if age >= 18:
     print("You are an adult")
     print("Yay!")
 print("Have a groovy day!")
+name = input("Enter your name: ")
+if name.upper() == "EMMA":
+    print("Awesome name!")
+
+#else
+grade = 60
+
+#if grade>= 50:
+   # print("Pass")#if true
+#else:
+   # print("Fail")#if false
+
+if grade >=80:
+    print("Honours")
+elif grade >=50:
+    print("Pass")
+elif grade ==0:
+    print("How did you get zero?")
+else: 
+    print("Fail!!!")
+
+
