7d7b00cf5f08430327d66c50e03fb80b3b2ec9cc
Emma Smitten <esmitten1@nait.ca>
2026-09-16 10:45:10 -0600

    added two files

---
+++ math_exercises.py
@@ -0,0 +1,67 @@
+# Math Exercises
+
+# Solve the following problems by identifying the steps the smaller problems first and then code the solution. Use the prompts and output that is showin in the sample runs that have been provided for each question.
+    
+# 1. Ask the user for a temperature in Celsius and convert it to Fahrenheit. Round the answer to one decimal place. 
+# Enter the temperature in Celsius: 32
+# 32.0 C is equal to 89.6 F.
+
+# 2. Ask the user for a number of miles and print out how many kilometres it is.  Display to 2 decimal places. The conversion rate is 1 mile = 1.609344 kilometers.
+# Enter the number of miles: 50
+# 50.0 miles is equal to 80.47 kilometres
+  
+
+# 3. One acre of land is equal to 43,560 square feet. Write a program that asks the user to enter the number of acres and display the number of square feet. Display to 2 decimal places
+# Enter the number of acres: 120
+# 120.0 acres is equal to 5227200.00 square feet.
+
+
+# 4. Write a program that asks the user to enter three test scores. The program should display each test score, as well as the average test of the users' scores.
+# Enter the first test score: 50
+# Enter the second test score: 60
+# Enter the third test score: 75
+
+# Test Score Summary
+# Test 1: 50.0
+# Test 2: 60.0
+# Test 3: 75.0
+
+# Average: 61.7
+
+
+# 5. A bag of cookies holds 40 cookies. The calorie information on the bag claims that there are 10 "servings" in the bag and that a serving equals 300 calories. Write a program that asks the user to input how many cookies they ate and then reports how many total calories were consumed.
+# How many cookies did you eat? 14
+# You consumed 1050 calories.
+
+
+# 6. Ask the user for:
+	
+# number of pizzas (int)
+# price per pizza (float, dollars)
+# tip percent (float; e.g., 15 for 15%)
+# number of people sharing (int)
+
+# Calculate and print: subtotal, tip amount, total, and amount per person.
+# formatting currency to 2 decimals. Display appropriate inputs and outputs.
+# Enter the number of pizzas: 6 
+# Enter the price per pizza: $20
+# Enter the tip percentage (for example, 15): 18
+# Enter the number of people sharing: 8
+
+# Pizza Bill
+# Subtotal:         $120.00
+# Tip:              $21.60
+# Total:            $141.60
+
+# Amount per person: $17.70
+
+
+# 7. Ask the user for a currency amount and display the number of dollars, quarters, dimes, nickels, and pennies in that amount.  
+# Enter a currency amount: $1.39
+
+# Currency Breakdown for $1.39
+# Dollars:  1
+# Quarters: 1
+# Dimes:    1
+# Nickels:  0
+# Pennies:  4
