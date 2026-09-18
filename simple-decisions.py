2387407e193132df5610ed547aac1e4b310c5071
Emma Smitten <esmitten1@nait.ca>
2026-09-18 10:32:49 -0600

    worked on simple-decisions.py during class

---
+++ simple-decisions.py
@@ -0,0 +1,23 @@
+# do for homework this weekend
+# email if you need help lol
+#Simple Decision Exercise
+#Use if/else in the following questions.
+#number1 = input("Enter your first number: ")
+#number2 = input("Enter your second number: ")
+#if number1 == number2:
+  #  (print("Numbers are equal!"))
+#elif number1 != number2:
+ #   print("Numbers are not equal!")
+
+number1 = input("Enter your first numner: ")
+number2 = input("Enter your second number: ")
+number3 = input("Enter your third number: ")
+
+#2. Prompt the user for two numbers and display the highest value.
+#3. Prompt the user for two numbers and display the highest value as well as display if it was the
+#second or first number entered.
+#4. Prompt the user for three numbers and display the highest value.
+#5. Prompt the user for a number and display a message indicating if it is even or odd.
+#6. Prompt the user for 2 numbers and a menu to allow them to choose to display the equation and
+#answer for adding, subtracting, multiplying or dividing the numbers.
+#Challenge: If the user enters an invalid operation, display an error message
