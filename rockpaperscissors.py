6236b9af6e2d28381ebc998ebda0a2ce79a55d89
Emma Smitten <esmitten1@nait.ca>
2026-09-21 11:42:10 -0600

    added rockpaperscissors.py file. need to work on and finish

---
+++ rockpaperscissors.py
@@ -0,0 +1,11 @@
+import random
+random_number = random.randint(0,2)
+scissors = 0
+rock = 1
+paper = 2
+
+print("Let's play rock, paper, scissors!!")
+print()
+result = int(input("Enter a number(0-2): "))
+
+print(random.randint)
