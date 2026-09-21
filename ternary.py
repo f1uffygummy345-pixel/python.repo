4c83e80df77749ab6c9f5e12c851b66370dfc224
Emma Smitten <esmitten1@nait.ca>
2026-09-21 11:02:17 -0600

    added random number file

--- ternary.py
+++ ternary.py
@@ -14,3 +14,16 @@ result = "pass" if grade >= 50 else "Fail"
 print(result)
 print("Pass" if grade >= 50 else "Fail")
 print()
+
+#even or odd
+number = int(input("Enter a number: "))
+result = "Even" if number % 2 == 0 else "Odd"
+print(result)
+
+
+
+#ask for two numbers and print the largest number
+number1 = (number1 if number1 > number2 else number2)
+print(largest)
+
+print(number1 if number1 > number2 else number2)
